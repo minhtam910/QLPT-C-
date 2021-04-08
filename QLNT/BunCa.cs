35e4350157237c1e9ac50541b91af93b08783fde
= <=>
2021-04-08 16:32:25 +0700

    Add Factory method

---
+++ QLNT/BunCa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLNT
+{
+    class BunCa:IService
+    {
+        public void cook()
+        {
+            Console.WriteLine("Bún cá đã được thêm vào dịch vụ");
+        }
+    }
+}
